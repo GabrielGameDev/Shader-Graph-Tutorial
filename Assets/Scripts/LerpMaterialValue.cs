85856b0147228bab64ae2617b2de8b8b9b9d8f67
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2019-06-10 20:28:36 -0300

    Cutscene Dissolve Shader
    Cutscene utilizando o shader dissolve. Alterando suas propriedades utilizando um trigger

---
+++ Assets/Scripts/LerpMaterialValue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpMaterialValue : MonoBehaviour
+{
+
+	public string property = "_DissolveValue";
+
+	private Renderer meshRenderer;
+
+	private float value;
+
+	private float desiredValue = 0;
+
+	private bool canLerp;
+
+	private float speed;
+
+	private void Awake()
+	{
+		meshRenderer = GetComponentInChildren<Renderer>();
+	}
+
+	void Update()
+	{
+
+		if (!canLerp)
+			return;
+
+		value = Mathf.Lerp(value, desiredValue, Time.deltaTime * speed);
+		meshRenderer.material.SetFloat(property, value);
+
+	}
+
+	public void SetLerp(float newValue, float newSpeed)
+	{
+		value = meshRenderer.material.GetFloat(property);
+		speed = newSpeed;
+		desiredValue = newValue;
+		canLerp = true;
+
+	}
+}
---
+++ Assets/Scripts/TriggerInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerInteraction : MonoBehaviour
+{
+	public float value;
+	public float speed = 1;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		LerpMaterialValue lerp = other.GetComponent<LerpMaterialValue>();
+		if (lerp != null)
+		{
+			lerp.SetLerp(value, speed);
+		}
+	}
+}

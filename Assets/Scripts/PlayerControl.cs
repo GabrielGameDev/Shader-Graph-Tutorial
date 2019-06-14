a38bfe16faf857364512419bf73362efc79041b8
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2019-06-14 14:21:43 -0300

    Vertex Displacement
    Shader de deslocamento de vértices

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	private Animator anim;
+
+	private VertexDisplacement sphere;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		sphere = FindObjectOfType<VertexDisplacement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Jump"))
+		{
+			anim.SetTrigger("Attack");
+		}   
+    }
+
+	
+	public void AttackSphere(float value)
+	{
+		sphere.amount = value;
+	}
+
+}
---
+++ Assets/Scripts/VertexDisplacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexDisplacement : MonoBehaviour
+{
+	public float amount;
+	public Light myLight;
+	private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		amount = Mathf.Lerp(amount, 0, Time.deltaTime);
+		meshRenderer.material.SetFloat("_amount", amount);
+
+		myLight.range = amount * 6;
+		myLight.intensity = amount * 24;
+		
+
+    }
+}
